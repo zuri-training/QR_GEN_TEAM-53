0ec2dea902746236b038a9098b3b76a437ed5d25
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-07-31 15:26:54 +0100

    Home and external website view

--- qrgen53/qrgen53/settings.py
+++ qrgen53/qrgen53/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts'
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
