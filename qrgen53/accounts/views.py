e32223b10a0652a35905a2784b4795694aabf30a
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-07-31 17:12:50 +0100

    Home and external website view

--- qrgen53/accounts/views.py
+++ qrgen53/accounts/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'accounts/index.html')
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+
     form = UserCreationForm()
     context = {'form' : form}
     return render(request, 'registration/register.html', context)
