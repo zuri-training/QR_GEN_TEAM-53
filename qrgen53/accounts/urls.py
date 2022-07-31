e16acd9af05478ba3d1b44e65b439aaa6541bc3e
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-07-31 23:06:31 +0100

    shift

--- qrgen53/accounts/urls.py
+++ qrgen53/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
