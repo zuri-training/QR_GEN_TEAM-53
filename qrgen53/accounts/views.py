52a9b442719acbf23922fd1ee4435d817644126b
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-08-05 20:47:53 +0100

    new pages, yet to be authorised

--- qrgen53/accounts/views.py
+++ qrgen53/accounts/views.py
@@ -19,7 +19,7 @@ def register_view(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            return redirect('')
+            return redirect('home')
     else:
         form = UserCreationForm()
 
