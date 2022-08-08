2ba43e705a2be5419aec297ed2981f403e1abd9f
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-08-08 18:53:21 +0100

    building frame

--- qrgen53/home/views.py
+++ qrgen53/home/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home_view(request):
     return render(request, 'index.html')
 
+
 def contact_view(request):
     return render(request, 'contact.html')
+
 
 def about_view(request):
     return render(request, 'about.html')
 
+
 def privacy_policy_view(request):
     return render(request, 'privacy-policy.html')
+
 
 def email_collection_view(request):
     return render(request, 'email-collection.html')
